278c1a3d50b6fc5a759e0c8a940bb39b52740c77
Junior Barros <junior.dnbj55@gmail.com>
2026-01-23 20:14:44 -0300

    adicioando Home

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home.jsx';
+import Login from './components/Login/Login.jsx';
  function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ import Home from './components/Home.jsx';
     <Header />
     <Routes>
       <Route path="/*" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       </Routes>
     <Footer />
     </BrowserRouter>
