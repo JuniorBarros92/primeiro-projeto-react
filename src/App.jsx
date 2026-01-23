4c49eada1980685007ea554753808eb2c7412915
Junior Barros <junior.dnbj55@gmail.com>
2026-01-23 15:56:47 -0300

    adiconando home, header,footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home.jsx';
  function App() {
   return (
     <div>
+      <BrowserRouter>
     <Header />
+    <Routes>
+      <Route path="/*" element={<Home />} />
+      </Routes>
     <Footer />
+    </BrowserRouter>
     </div>
   );
 }
