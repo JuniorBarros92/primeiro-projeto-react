5086601dd575daf4197481c5c80cb9f92a89eaf5
Junior Barros <junior.dnbj55@gmail.com>
2026-01-29 21:23:22 -0300

    estilização pagina login

--- src/components/Login/loginForm.jsx
+++ src/components/Login/loginForm.jsx
@@ -5,6 +5,8 @@ import Button from '../Forms/Button';
 import useUserForm from '../../Hooks/useUserForm';
 import { UserContext } from '../../UserContext';
 import Error from '../Helper/Error';
+import styles from'./LoginForm.module.css';
+import stylesBtn from'../Forms/Button.module.css';
 
 const LoginForm = () => {
   const username = useUserForm();
@@ -23,7 +25,7 @@ const LoginForm = () => {
   return (
     <section className="animeLeft">
       <h1 className='title'>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+      <form className={styles.form} action="" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         {loading ? (
@@ -35,7 +37,13 @@ const LoginForm = () => {
         <Error error={error} />
         
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+      <Link className={styles.perdeu} to="/login/perdeu">Perdeu a Senha?</Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não tem conta? Cadastre-se agora!</p>
+      
+      <Link className={stylesBtn.button} to="/login/criar">Cadastro</Link>
+      </div>
     </section>
   );
 };
