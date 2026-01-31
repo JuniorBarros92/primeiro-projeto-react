d8870401bbb6bde4b574ed8659a91a5adf429b9d
Junior Barros <junior.dnbj55@gmail.com>
2026-01-31 17:14:47 -0300

    user header

--- src/components/User/UserHeader.jsx
+++ src/components/User/UserHeader.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import UserHeaderNav from './UserHeaderNav';
 import styles from './UserHeader.module.css';
 
 
 const UserHeader = () => {
+
+const [title, setTitle] = React.useState('');
+const location = useLocation();
+React.useEffect(() => {
+ 
+    const { pathname } = location;
+    switch (pathname) {
+        case '/conta/postar':
+            setTitle('Postar sua Foto');
+            break;
+        case '/conta/estatisticas':
+            setTitle('Estatísticas');
+            break;
+        default:
+            setTitle('Minhas Fotos');
+    }
+  }, [location]);
+
   return (
    
     <header className={styles.header}>
-        <h1 className={styles.title}>Minha Conta</h1>
+        <h1 className={styles.title}>{title}</h1>
         <UserHeaderNav />
     </header>
   );
--- src/components/User/UserHeaderNav.jsx
+++ src/components/User/UserHeaderNav.jsx
@@ -1,35 +1,38 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { UserContext } from "../../UserContext";
-import MinhasFotos from "../../Assets/feed.svg?react";
-import Estatisticas from "../../Assets/estatisticas.svg?react";
-import AdiconarFoto from "../../Assets/adicionar.svg?react";
-import Sair from "../../Assets/sair.svg?react";
-import styles from "./UserHeaderNav.module.css";
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import MinhasFotos from '../../Assets/feed.svg?react';
+import Estatisticas from '../../Assets/estatisticas.svg?react';
+import AdicionarFoto from '../../Assets/adicionar.svg?react';
+import Sair from '../../Assets/sair.svg?react';
+import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
-
-    const [mobileMenu, setMobileMenu] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
+  const navigate = useNavigate();
+  function handleLogout() {
+    userLogout();
+    navigate('/login');
+  }
+  const [mobile, setMobile] = React.useState(null);
 
   return (
     <nav className={styles.nav}>
       <NavLink to="/conta" end>
         <MinhasFotos />
-        {mobileMenu && "Minhas fotos"}
+        {mobile && 'Minhas Fotos'}
       </NavLink>
       <NavLink to="/conta/estatisticas">
         <Estatisticas />
-        {mobileMenu && "Estatísticas"}
+        {mobile && 'Estatísticas'}
       </NavLink>
       <NavLink to="/conta/postar">
-        <AdiconarFoto />
-        {mobileMenu && "Adicionar Foto"}
+        <AdicionarFoto />
+        {mobile && 'Adicionar Foto'}
       </NavLink>
-
-      <button onClick={userLogout}>
-        {" "}
-        <Sair /> {mobileMenu && "Sair"}
+      <button onClick={handleLogout}>
+        <Sair />
+        {mobile && 'Sair'}
       </button>
     </nav>
   );
