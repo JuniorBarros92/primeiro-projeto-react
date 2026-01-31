d0a72450f75659edac36036d7000b7a910720c3c
Junior Barros <junior.dnbj55@gmail.com>
2026-01-30 22:24:35 -0300

    adiconando userFetch

--- src/components/Login/LoginCreate.jsx
+++ src/components/Login/LoginCreate.jsx
@@ -4,45 +4,36 @@ import Button from "../Forms/Button";
 import useUserForm from "../../Hooks/useUserForm";
 import { USER_POST } from "../../api";
 import Error from "../Helper/Error";
-import styles from "./Login.module.css";
 import { UserContext } from "../../UserContext";
+import userFetch from "../../Hooks/userFetch";
 
 const LoginCreate = () => {
   const username = useUserForm();
   const email = useUserForm("email");
   const password = useUserForm();
-  const [error, setError] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
 
-
-  const {userLogin} = React.useContext(UserContext);
-
+  const { userLogin } = React.useContext(UserContext);
+  const { loading, error, request } = userFetch();
+  
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && email.validate() && password.validate()) {
       try {
-        setError(null);
-        setLoading(true);
         const { url, options } = USER_POST({
           username: username.value,
           email: email.value,
           password: password.value,
         });
-        const response = await fetch(url, options);
-        if (!response.ok)userLogin(username.value, password.value);
-        
-        if (!response.ok) {
-          const json = await response.json();
-          throw new Error(json.message || "Erro ao cadastrar");
+        const { response, json } = await request(url, options);
+        if (response && response.ok) {
+          userLogin(username.value, password.value);
+          console.log("Cadastro realizado com sucesso!");
+        } else {
+          console.error(json || "Erro ao cadastrar");
         }
-        
-        // Sucesso - redirecionar ou limpar formulário
-        console.log("Cadastro realizado com sucesso!");
       } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
+        console.error(err);
       }
     }
   }
