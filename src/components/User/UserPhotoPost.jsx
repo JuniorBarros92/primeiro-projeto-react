5e4aafd33ff2f689f4a975054174b534b9c6ea2b
Junior Barros <junior.dnbj55@gmail.com>
2026-02-04 22:08:59 -0300

    iniciando pagina fotos

--- src/components/User/UserPhotoPost.jsx
+++ src/components/User/UserPhotoPost.jsx
@@ -1,10 +1,28 @@
-import React from "react";  
-
+import React from "react";
+import styels from "./UserPhotoPost.module.css";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
 
 const UsePhotoPost = () => {
-  return ( 
-    <div>Postar Foto</div>
-    );
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleImgChange() {
+    // handle image change
+  }
+
+  return (
+    <section className={`${styels.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="nome" />
+        <Input label="Peso" type="text" name="peso" />
+        <Input label="Idade" type="text" name="idade" />
+        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <Button>Enviar</Button>
+      </form>
+    </section>
+  );
+};
 
 export default UsePhotoPost;
