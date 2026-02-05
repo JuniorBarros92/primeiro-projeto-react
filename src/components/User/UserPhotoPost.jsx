9fdd29c03a6ff73a2b17d798aa643d9d054d3fed
Junior Barros <junior.dnbj55@gmail.com>
2026-02-04 22:24:07 -0300

    melhorias pagina adicionar foto

--- src/components/User/UserPhotoPost.jsx
+++ src/components/User/UserPhotoPost.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import styels from "./UserPhotoPost.module.css";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
 
 const UsePhotoPost = () => {
+const nome = useForm();
+const peso = useForm("number");
+const idade = useForm("number");
+const [img, setImg] = React.useState({});
+const { data, error, loading, request } = useFetch();
+
   function handleSubmit(event) {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append("img", img.raw);
+    formData.append("nome", nome.value);
+    formData.append("peso", peso.value);
+    formData.append("idade", idade.value);
+ 
   }
 
-  function handleImgChange() {
-    // handle image change
+  function handleImgChange({ target }) {
+    setImg({
+      preview: URL.createObjectURL(target.files[0]),
+      raw: target.files[0]
+    });
   }
 
   return (
