e6ed3ffaf2fd7c3ddb9f1b8bcf04a0e1f85fb39b
Junior Barros <junior.dnbj55@gmail.com>
2026-02-14 15:18:17 -0300

    Feed Modal 1

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import FeedModal from "./FeedModal";
-import FeedPhotos from "./FeedPhotos";
+import React from 'react';
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
 
 const Feed = () => {
-  return <div>
-<FeedModal />
-<FeedPhotos />
+  const [modalPhoto, setModalPhoto] = React.useState(null);
 
-  </div>;
+  return (
+    <div>
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
+    </div>
+  );
 };
+
 export default Feed;
