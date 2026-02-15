418378225b4a37f1af870094a4b5be87fcc9b6df
Junior Barros <junior.dnbj55@gmail.com>
2026-02-15 15:36:13 -0300

    Perfil Usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 import User from './components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './components/Photo/Photo';
+import UserProfile from './components/User/UserProfile';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login/*" element={<Login />} />
-        <Route
-          path="conta/*"
-          element={
-            <ProtectedRoute>
-              <User />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="foto/:id" element={<Photo />} />
-      </Routes>
-      <Footer />
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
   );
 }
