b88b5285ce9b718b814ef9d43d49b42c3cd793e5
Junior Barros <junior.dnbj55@gmail.com>
2026-02-15 13:43:57 -0300

    Scroll Infinito

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 import UserHeader from './UserHeader';
+import { Routes, Route } from 'react-router-dom';
 import Feed from '../Feed/Feed';
 import UserPhotoPost from './UserPhotoPost';
 import UserStarts from './UserStarts';
+import { UserContext } from '../../UserContext';
 
 const User = () => {
-  return <section className='container'>
-    <UserHeader />
-    <Routes>
-   
-   <Route path='/' element={<Feed />} />
-   <Route path='postar' element={<UserPhotoPost />} />
-   <Route path='estatisticas' element={<UserStarts />} />
-    </Routes>
+  const { data } = React.useContext(UserContext);
 
-  </section>
-};  
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed user={data.id} />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStarts />} />
+      </Routes>
+    </section>
+  );
+};
 
 export default User;
